1ecb69cbbd3f83e7fa8641662dd67fb5f4197403
sungjin <ysoon0912@gmail.com>
2023-05-14 23:53:27 +0900

    Complete toggleRead, removeBookFromLibrary

--- script.js
+++ script.js
@@ -2,24 +2,20 @@ function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = isRead ? "Read" : "Unread";
+  this.read = isRead;
 }
+let myLibrary = [];
 
 const addButton = document.querySelector(".add");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const submitButton = document.querySelector(".submit");
 const mainContent = document.querySelector(".main-content");
+const addBookForm = document.getElementById("addBook");
 
 addButton.addEventListener("click", showModal);
 overlay.addEventListener("click", closeModal);
 submitButton.addEventListener("click", addBookToLibrary);
-
-function logConsole(e) {
-  console.log(e);
-}
-
-let myLibrary = [];
 
 function addBookToLibrary(event) {
   event.preventDefault();
@@ -33,34 +29,67 @@ function addBookToLibrary(event) {
   closeModal();
 }
 
-Book.prototype.info = function () {
-  return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-};
+function toggleRead(e) {
+  const title = e.target.parentNode.firstChild.innerText;
+  for (const book of myLibrary) {
+    if (book.title == title) {
+      book.read = !book.read;
+    }
+  }
+  updateGrid();
+}
 
-Book.prototype.toggleRead = function () {
-  this.read == "Read" ? (this.read = "Unread") : (this.read = "Read");
-};
-
-const myBook1 = new Book("Hobbit", "J.R.R", 295);
-const myBook2 = new Book("Harry Potter", "J.K", 15);
+function removeFromLibrary(e) {
+  const title = e.target.parentNode.firstChild.innerText;
+  for (const book of myLibrary) {
+    if (book.title == title) {
+      const index = myLibrary.indexOf(book);
+      myLibrary.splice(index, 1);
+    }
+  }
+  updateGrid();
+}
 
 function updateGrid() {
   mainContent.innerHTML = "";
   for (const book of myLibrary) {
-    const div = document.createElement("div");
-    div.setAttribute("class", "card");
-    div.innerHTML = `
-          <div class="title">${book.title}</div>
-          <div class="author">${book.author}</div>
-          <div class="pages">${book.pages}</div>
-          <button class="read">${book.read}</button>
-          <button class="remove">Remove</button>
-        `;
-    mainContent.append(div);
+    const container = document.createElement("div");
+    const title = document.createElement("div");
+    const author = document.createElement("div");
+    const pages = document.createElement("div");
+    const read = document.createElement("button");
+    const remove = document.createElement("button");
+
+    container.classList.add("card");
+    title.classList.add("title");
+    author.classList.add("author");
+    pages.classList.add("pages");
+    read.classList.add("read");
+    remove.classList.add("remove");
+
+    title.innerText = book.title;
+    author.innerText = book.author;
+    pages.innerText = book.pages;
+    read.innerText = book.read == true ? "Read" : "Unread";
+    remove.innerText = "Remove";
+
+    container.append(title);
+    container.append(author);
+    container.append(pages);
+    container.append(read);
+    container.append(remove);
+
+    mainContent.append(container);
+
+    const readButton = document.querySelector(".read");
+    readButton.addEventListener("click", toggleRead);
+    const removeButton = document.querySelector(".remove");
+    removeButton.addEventListener("click", removeFromLibrary);
   }
 }
 
 function showModal() {
+  addBookForm.reset();
   modal.classList.add("active");
   overlay.classList.add("active");
 }
@@ -68,5 +97,3 @@ function closeModal() {
   modal.classList.remove("active");
   overlay.classList.remove("active");
 }
-
-updateGrid();
